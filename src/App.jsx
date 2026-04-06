9cbbfc6e078047cec46aa11596e82ef7b30a19c0
Ramil Deocariza Jr. <ramildeocariza009@gmail.com>
2026-04-06 16:13:13 +0800

    Add login page UI with email/password and Google OAuth

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
-<Routes>
-  {/* Public */}
-  <Route path="/login" element={<LoginPlaceholder />} />
-  <Route path="/auth/callback" element={<AuthCallback />} />
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login'; 
 
-  {/* Private - Redirects to /login if session is null */}
-  <Route element={<ProtectedRoute />}>
-    <Route path="/employees" element={<Employees />} />
-    <Route path="/jobhistory" element={<JobHistory />} />
-    <Route path="/jobs" element={<Jobs />} />
-    <Route path="/departments" element={<Departments />} />
-    <Route path="/admin" element={<Admin />} />
-    <Route path="/deleted-items" element={<DeletedItems />} />
-    
-    {/* Default logged-in landing page */}
-    <Route path="/" element={<Navigate to="/employees" replace />} />
-  </Route>
+import ProtectedRoute from './components/ProtectedRoute';
+import AuthCallback from './pages/AuthCallback';
+import Employees from './pages/Employees';
+import JobHistory from './pages/JobHistory';
+import Jobs from './pages/Jobs';
+import Departments from './pages/Departments';
+import Admin from './pages/Admin';
+import DeletedItems from './pages/DeletedItems';
 
-  {/* Catch-all for any undefined route */}
-  <Route path="*" element={<Navigate to="/login" replace />} />
-</Routes>
+export default function App() {
+  return (
+    <Routes>
+      {/* Public */}
+      {/* Update this line to use your new Login component */}
+      <Route path="/login" element={<Login />} /> 
+      <Route path="/auth/callback" element={<AuthCallback />} />
+
+      {/* Private - Redirects to /login if session is null */}
+      <Route element={<ProtectedRoute />}>
+        <Route path="/employees" element={<Employees />} />
+        <Route path="/jobhistory" element={<JobHistory />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/departments" element={<Departments />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/deleted-items" element={<DeletedItems />} />
+        
+        {/* Default logged-in landing page */}
+        <Route path="/" element={<Navigate to="/employees" replace />} />
+      </Route>
+
+      {/* Catch-all for any undefined route */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  );
+}
